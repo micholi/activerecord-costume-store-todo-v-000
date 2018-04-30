99e7f1feeb12a1c22793eed5596638dc88c8d5fa
Michelle Olivieri <michelleo72@gmail.com>
2018-04-30 15:44:02 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
     end
   end
 
