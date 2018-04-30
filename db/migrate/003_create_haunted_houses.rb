5d38e30d2df2e691f843dd991f9039404f43b09d
Michelle Olivieri <michelleo72@gmail.com>
2018-04-30 15:42:35 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,8 +3,13 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :costume_stores do |t|
-
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      
     end
   end
 
